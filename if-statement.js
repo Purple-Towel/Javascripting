9cc9c72f419c66a2f5ba6d53f5279788b43f3880
Angad Grewal <angadgrewal31@gmail.com>
2020-03-07 20:36:38 +0000

    all files from javascripting excercise

---
+++ if-statement.js
@@ -0,0 +1,8 @@
+fruit = 'orange'
+fruitLength = fruit.length
+
+if (fruitLength > 5) {
+  console.log('The fruit name has more than five characters.')
+} else  {
+  console.log('The fruit name has five or less characters.')
+}
